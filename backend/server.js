d6c174e13965d54afd4aa399f7bd4068fe932182
Jonny Graybill <jon.a.graybill@gmail.com>
2020-10-15 21:03:31 -0700

    profile route working correctly

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,43 @@
-const cors = require("cors");
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
-// import routes
+
+// routes
 const blogRoutes = require('./routes/blog');
 const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
 
 // app
 const app = express();
 
-// database
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true }).then(() => console.log('Database connected'));
+// db
+mongoose
+  .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+  .then(() => console.log('Database connected'))
+  .catch(err => {
+    console.log(err);
+  });
 
 // cors
 if (process.env.NODE_ENV === 'development') {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
 
-// middleware
+// middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
 // routes middleware
 app.use('/api', blogRoutes);
 app.use('/api', authRoutes);
-
+app.use('/api', userRoutes);
 
 // port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+  console.log(`Server is running on port ${port}`);
 });
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9cadfcc2a5451bfe04b4";
+/******/ 	var hotCurrentHash = "75712b0a884b5928f99b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
