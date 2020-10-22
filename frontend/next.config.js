74fca79b911ec9abaa009da2055a9b0880156715
Jonny Graybill <jon.a.graybill@gmail.com>
2020-10-21 18:03:01 -0700

    full blog crud backend functionality

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,11 @@
 // Environmental variables
 
-module.exports = {
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
   publicRuntimeConfig: {
     APP_NAME: 'SEOBLOG',
     API_DEVELOPMENT: 'http://localhost:8000/api',
     PRODUCTION: false
   }
-};
+});
