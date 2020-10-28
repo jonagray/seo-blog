1a09443a2f6e36860681344202a37c22075a6331
Jonny Graybill <jon.a.graybill@gmail.com>
2020-10-27 18:09:09 -0700

    completed categories and tags seo

--- backend/controllers/tag.js
+++ backend/controllers/tag.js
@@ -1,6 +1,7 @@
 const Tag = require('../models/tag');
+const Blog = require('../models/blog');
 const slugify = require('slugify');
-const {errorHandler} = require('../helpers/dbErrorHandler');
+const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.create = (req, res) => {
   const { name } = req.body;
@@ -34,20 +35,33 @@ exports.list = (req, res) => {
 exports.read = (req, res) => {
   const slug = req.params.slug.toLowerCase();
 
-  Tag.findOne({slug}).exec((err, tag) => {
+  Tag.findOne({ slug }).exec((err, tag) => {
     if (err) {
       return res.status(400).json({
         error: errorHandler(err)
       });
     };
-    res.json(tag);
+    // res.json(tag);
+    Blog.find({ tags: tag })
+      .populate('categories', '_id name slug')
+      .populate('tags', '_id name slug')
+      .populate('postedBy', '_id name')
+      .select('_id title slug excerpt categories postedBy tags createdAt updatedAt')
+      .exec((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        }
+        res.json({ tag: tag, blogs: data })
+      })
   });
 };
 
 exports.remove = (req, res) => {
   const slug = req.params.slug.toLowerCase();
 
-  Tag.findOneAndRemove({slug}).exec((err, data) => {
+  Tag.findOneAndRemove({ slug }).exec((err, data) => {
     if (err) {
       return res.status(400).json({
         error: errorHandler(err)
